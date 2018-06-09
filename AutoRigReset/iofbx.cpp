127e73ec2e31cdff8329e2277e716c0c999932eb
Leonid <lion6149@mail.ru>
2018-06-09 15:51:33 +0300

    joints + posenode

--- AutoRigReset/iofbx.cpp
+++ AutoRigReset/iofbx.cpp
@@ -12,6 +12,11 @@ IOfbx::FbxGeometryMesh::FbxGeometryMesh()
     polygonVertexIndex = QVector<int>();
 }
 
+void IOfbx::FbxModelJoint::addSubName(QString subName)
+{
+    this->subName = subName;
+}
+
 QString IOfbx::FbxGeometryMesh::parse(QStringList S, const int param)
 {
     QString error = QString();
@@ -34,6 +39,49 @@ QString IOfbx::FbxGeometryMesh::parse(QStringList S, const int param)
     qDebug() << QString("v   Success parsed geometry mesh values;");
     return error;
 }
+QString IOfbx::FbxModelJoint::parse(QStringList S, const int param)
+{
+    QString error = QString();
+    for (int lineInd = 0; lineInd < S.length(); lineInd++){
+        QString line = S[lineInd];
+        if (line.indexOf("\"Lcl") > 0){
+            QVector<double> values = parseLastXValues<double>(line, 3, error);
+            if (!error.isEmpty())
+                return "Error in parsing 3 last values, error in line: " + line + "; " + error;
+            if (line.indexOf("Lcl Translation") >= 0)
+                this->localTranslation = values;
+            if (line.indexOf("Lcl Rotation") >= 0)
+                this->localRotation = values;
+            if (line.indexOf("Lcl Scaling") >= 0)
+                this->localScaling = values;
+        }
+    }
+    qDebug() << QString("v   Success parsed joint " + this->name);
+    return QString();
+}
+
+QString IOfbx::FbxPoseNode::parse(QStringList S, const int param)
+{
+//        PoseNode:  {
+//			Node: 1848447504
+//			Matrix: *16 {
+//				a: 0.393700778484344,0,0,0,0,0.393700778484344,1.7209208280633e-008,0,0,-1.7209208280633e-008,0.393700778484344,0,-0.000393700785934925,45.167106628418,-0.192725494503975,1
+//			}
+    QStringList arrayContainer = QStringList();
+    for (int i = 0; i < S.length(); i++)
+        if (S[i].indexOf("Matrix:") >= 0)
+        {
+            QString error = "";
+            transformMatrixArray = parseFbxArray<double>(QStringList(S[i + 1]), error);
+            if (!error.isEmpty())
+                return "Error in parsing 16 values in matrix of posenode, error in line: " + S[i] + "; " + error;
+            break;
+        }
+
+    qDebug() << QString("v   Success parsed posenode;");
+    return QString();
+}
+
 
 
 QString IOfbx::loadFromPath(const QString path)
@@ -57,11 +105,14 @@ QString IOfbx::loadFromPath(const QString path)
     FbxGeometryMesh fbxMesh = FbxGeometryMesh();   // a readen date of mesh
     QVector<FbxModelJoint> fbxJoints = QVector<FbxModelJoint>();    // a joints readen
     fbxJoints << FbxModelJoint();
+    QVector<FbxPoseNode> fbxPoseNodes = QVector<FbxPoseNode>();
+    fbxPoseNodes << FbxPoseNode();
 
     // read a file line by line
     QString line = "";
     QString nodeId = "noneID";
     QString nodeName = "unknown";
+    QString nodeSubName = "unknown";
     QStringList nodeBuffer = QStringList();
     ParseType lastParsingType = ParseType::None;
 
@@ -70,7 +121,7 @@ QString IOfbx::loadFromPath(const QString path)
         ParseType detectedTypeHeader = pushHeader(line);
 
         // there is sometimes a need to remember an ID of node, you are currently parsing
-        findIdAndNameInLine(line, nodeId, nodeName);
+        findIdAndNameInLine(line, nodeId, nodeName, nodeSubName);
 
         if (detectedTypeHeader != ParseType::None){
             // if it is first detected, then clear buffer and start copying
@@ -105,10 +156,20 @@ QString IOfbx::loadFromPath(const QString path)
                             return "x   Error in parsing a mesh polygon idnexes: " + err;
                         break;
                     case ParseType::FbxObjectModelLimbNodeProperty:
-                        err = // [i].Parse()
-                                //  << to arr
-                                // << some new :3
-                                // remember to clean last zero on end
+                        if (!fbxJoints.last().hasNameAndID())
+                            fbxJoints.last().setNameAndID(nodeName, nodeId);
+                        err = fbxJoints.last().parse(nodeBuffer, 0);
+                        fbxJoints.last().addSubName(nodeSubName);
+
+                        if (!err.isEmpty())
+                            return "x   Error in parsing a joint: " + err;
+                        fbxJoints << FbxModelJoint();
+                        break;
+                    case ParseType::FbxObjectPoseNodeID:
+                        err = fbxPoseNodes.last().parse(nodeBuffer, 0);
+                        if (!err.isEmpty())
+                            return "x   Error in parsing a posenode: " + err;
+                        fbxPoseNodes << FbxPoseNode();
                         break;
                     default:
                         break;
@@ -119,6 +180,8 @@ QString IOfbx::loadFromPath(const QString path)
 
         lastParsingType = detectedTypeHeader;
     }
+    // post makes
+    fbxJoints.removeAt(fbxJoints.length() - 1);
 
     // v   Success and exit
     qDebug() << "v   File was succesfully loaded : " + fileName;
@@ -156,10 +219,13 @@ IOfbx::ParseType IOfbx::pushHeader(const QString fromLine)
     if (indexOfHeaders({"Geometry", "PolygonVertexIndex"}) > 0)
         return ParseType::FbxGeometryMeshPolygonIndexes;
 
-    if (indexOfHeaders({"Model","Properties70"}) > 0/* && fromLine.indexOf("LimbNode") > 0*/)
+    if (indexOfHeaders({"Model","Properties70"}) > 0)
         return ParseType::FbxObjectModelLimbNodeProperty;
-
-    return ParseType::None;
+    if (indexOfHeaders({"Pose","PoseNode"}) > 0)
+        return ParseType::FbxObjectPoseNodeID;
+    if (indexOfHeaders({"Pose","PoseNode","Matrix"}) > 0)
+        return ParseType::FbxObjectPoseNodeMatrix;
+    return ParseType::  None;
 }
 
 int IOfbx::indexOfHeaders(const QVector<QString> tags)
@@ -171,9 +237,6 @@ int IOfbx::indexOfHeaders(const QVector<QString> tags)
     for (int i = stackHeaders.length() - tags.length(), j = 0; i < stackHeaders.length(); i++, j++)
         if (stackHeaders[i] != tags[j])
             return -1;
-//    QString res = "";
-//    for (int i = 0; i < stackHeaders.length(); i++)
-//        res += stackHeaders[i] + " _ ";
 
     return stackHeaders.length() - tags.length();
 }
@@ -189,29 +252,55 @@ QVector<T> IOfbx::FbxNode::parseFbxArray(QStringList S, QString &error)
         qDebug() << "+   Warning: array parser is using for non-array value.";
 
     for (int lineIndex = 0; lineIndex < S.length(); lineIndex++){
-        S[lineIndex] = S[lineIndex].trimmed();                                  // rmove tabs
+        S[lineIndex] = S[lineIndex].trimmed();                                  // remove tabs
         if (S[lineIndex].indexOf("a: ") == 0) S[lineIndex] = S[lineIndex].remove(0,3);  // remove a:
         if (S[lineIndex][S[lineIndex].length() - 1] == ',') S[lineIndex].remove(S[lineIndex].length() - 2, 1) ;
 
-        QStringList splitedNumbers = S[lineIndex].split(',');
-        // case of incorrect format
-        if (splitedNumbers.length() <= 0)
-        {error = "Line " + S[lineIndex] + " does not contain any T values!"; return readenNumbers;}
-        // okay, the line is splited, so it is time to parse
-        bool numberParsedCorrectly = false;
-        for (int numIndex = 0; numIndex < splitedNumbers.length(); numIndex++){
-            // remove different tabs and spaces
-            splitedNumbers[numIndex] = splitedNumbers[numIndex].trimmed();
-            if (splitedNumbers[numIndex] != "")
-            {
-                const auto parsedValue = T(splitedNumbers[numIndex].toDouble(&numberParsedCorrectly));
-                if (!numberParsedCorrectly)
-                {error = "Can not parse a value from  \"" +splitedNumbers[numIndex] + "\"!" ; return readenNumbers;}
-                readenNumbers << parsedValue;
-            }
+        QVector<T> fromLine = parseLastXValues<T>(S[lineIndex], -1, error);
+        if (!error.isEmpty())
+        {return readenNumbers;}
+        for (int valInd = 0; valInd < fromLine.length(); valInd++)
+            readenNumbers << fromLine[valInd];
+    }
+    qDebug() << "v   Success: array parser read "
+                +QString::number( readenNumbers.length()) + " values;";
+    return readenNumbers;
+}
+
+template<typename T>
+QVector<T> IOfbx::FbxNode::parseLastXValues(const QString S, const int X, QString &error)
+{
+    QVector<T> readenNumbers = QVector<T>();
+    error = "";
+    if (S.indexOf(',') < 0)
+    {error = "Line does not have any \",\""; return readenNumbers;}
+
+    QStringList splitedNumbers = S.split(',');
+    // case of incorrect format
+    if (splitedNumbers.length() <= 0)
+    {error = "Line " + S + " does not contain any T values!"; return readenNumbers;}
+    if (splitedNumbers.length() < X)
+    {error = "Line " + S + " does not contain at least "+X+" values! ( it only contains " + splitedNumbers.length() + " )"; return readenNumbers;}
+
+    // okay, the line is splited, so it is time to parse
+    bool numberParsedCorrectly = false;
+    int count = X;
+    if (count < 0) count = splitedNumbers.length();
+    for (int numIndex = splitedNumbers.length() - count;
+         numIndex < splitedNumbers.length();
+         numIndex++)
+    {
+        // remove different tabs and spaces
+        splitedNumbers[numIndex] = splitedNumbers[numIndex].trimmed();
+        if (splitedNumbers[numIndex] != "")
+        {
+            const auto parsedValue = T(splitedNumbers[numIndex].toDouble(&numberParsedCorrectly));
+            if (!numberParsedCorrectly)
+            {error = "Can not parse a value from  \""
+                     +splitedNumbers[numIndex] + "\"!" ; return readenNumbers;}
+            readenNumbers << parsedValue;
         }
     }
-    qDebug() << "v   Success: array parser read " +QString::number( readenNumbers.length()) + " values;";
     return readenNumbers;
 }
 
@@ -231,11 +320,14 @@ void IOfbx::FbxNode::traceInfo() const
     qDebug() << "Name: " + name + ";   ID: " + id + ";";
 }
 
-void IOfbx::findIdAndNameInLine(const QString line, QString &id, QString &name)
+void IOfbx::findIdAndNameInLine(const QString line, QString &id, QString &name, QString &subName)
 {
     QString ableIdCharacters = "1234567890";
-    if (line.indexOf("{") > line.indexOf(",") && line.indexOf(",") > line.indexOf(":") && line.indexOf(":") >= 0){
-        QString canBeNodeId = line.mid(line.indexOf(":") + 2, line.indexOf(",") - line.indexOf(":") - 2);
+    if (line.indexOf("{") > line.indexOf(",")
+            && line.indexOf(",") > line.indexOf(":")
+            && line.indexOf(":") >= 0){
+        QString canBeNodeId =
+                line.mid(line.indexOf(":") + 2, line.indexOf(",") - line.indexOf(":") - 2);
         bool isTrueId = true;
         for (int i = 0; i < canBeNodeId.length(); i++)
             if (ableIdCharacters.indexOf( canBeNodeId[i] ) < 0)
@@ -243,13 +335,19 @@ void IOfbx::findIdAndNameInLine(const QString line, QString &id, QString &name)
         if (isTrueId){
             // set a true, beautiful ID
             id = canBeNodeId;
-            // also try to get a name
-            int firstBracInd = line.indexOf('\"'), secondBracInd = -1;
-            // get a name betw brackets
-            if (firstBracInd >= 0){
-                secondBracInd = line.indexOf('\"', firstBracInd+1);
-                if (secondBracInd > 0)
-                    name = line.mid(firstBracInd, secondBracInd-firstBracInd);
+            //  Model: 1852460320, "Model::RightToe_End", "LimbNode" {
+            QStringList s = line.split(',');
+            if (s.length() >= 3){
+                for (int i = 1; i < 3; i++){
+                    s[s.length() - i] = s[s.length() - i].trimmed();
+                }
+                subName = s[s.length() - 1];
+                subName.remove(0,1);
+                subName.remove(subName.length() - 3, 3);
+                name = s[s.length() - 2];
+                name.remove(0,1);
+                name.remove(name.length() - 1, 1);
+                int X = 10;
             }
         }
     }
@@ -262,3 +360,12 @@ IOfbx::FbxModelJoint::FbxModelJoint()
     localRotation = QVector<double>();
     localScaling = QVector<double>();
 }
+
+
+
+
+IOfbx::FbxPoseNode::FbxPoseNode()
+{
+    transformMatrixArray = QVector<double>();
+}
+
--- AutoRigReset/iofbx.h
+++ AutoRigReset/iofbx.h
@@ -6,13 +6,20 @@
 
 namespace IOfbx {
 
-static QVector<QString> parseBlockNames = {"Geometry Mesh Vertices", "Geometry Mesh Polygon Indexes", "Object Model Limb Node info", "???"};
+static QVector<QString> parseBlockNames = {"Geometry Mesh Vertices",
+                                           "Geometry Mesh Polygon Indexes",
+                                           "Object Model Limb Node info",
+                                           "Object Pose Node Id",
+                                           "Object Pose Node Bind Matrix",
+                                           "???"};
 enum class ParseType
 {
     None = -1,
     FbxGeometryMeshVertices = 0,
     FbxGeometryMeshPolygonIndexes = 1,
     FbxObjectModelLimbNodeProperty = 2,
+    FbxObjectPoseNodeID = 3,
+    FbxObjectPoseNodeMatrix = 4,
 };
 
 class FbxNode
@@ -21,6 +28,8 @@ public:
     virtual QString parse(QStringList S, const int param) = 0;
     template <typename T>
     static QVector<T> parseFbxArray(QStringList S, QString& error);
+    template <typename T>
+    static QVector<T> parseLastXValues (const QString S, const int X, QString& error);
     void setNameAndID (const QString name, const QString ID);
     bool hasNameAndID () const;
     virtual void traceInfo() const;
@@ -43,10 +52,22 @@ class FbxModelJoint : public FbxNode
 {
 public:
     FbxModelJoint ();
+    void addSubName (QString subName);
+    QString parse(QStringList S, const  int param) override;
 private:
+    QString subName;
     QVector<double> localTranslation;
     QVector<double> localRotation;
     QVector<double> localScaling;
+};
+
+class FbxPoseNode : public FbxNode
+{
+public:
+    FbxPoseNode();
+    QString parse(QStringList S, const int param) override;
+private:
+    QVector<double> transformMatrixArray;
 };
 
 // _-_-_-_-_-_-_-_-_-_-PARSER-_-_-_-_-_-_-_-_-_-_-_-_
@@ -58,7 +79,7 @@ int indexOfHeaders (const QVector<QString> tags);
 
 // the method of loading
 QString loadFromPath(const QString path);
-void findIdAndNameInLine (const QString line, QString& id, QString& name);
+void findIdAndNameInLine (const QString line, QString& id, QString& name, QString &subName);
 }
 
 #endif // IOFBX_H
